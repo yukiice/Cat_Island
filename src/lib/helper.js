dd6610f09193e2a736e1a3c5067f1caf222d566f
yukiice <58852690+yukiice@users.noreply.github.com>
2021-03-03 20:14:15 +0800

    去除杂项

--- src/lib/helper.js
+++
@@ -1,7 +0,0 @@
-function success(msg,errorCode){
-    throw new global.errs.Success()
-}
-
-module.exports = {
-    success
-}
--- src/routes/classic.js
+++
@@ -1,9 +0,0 @@
-const Router = require('koa-router')
-const {Auth} = require('../middleware/auth')
-const router = new Router()
-
-router.get('/latest',new Auth(9).m, async (ctx,next)=>{
-    ctx.body = ctx.auth.uid
-} )
-
-module.exports = router
